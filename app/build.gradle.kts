4d59d21751e4729df055fbf11bd4d1c83dc190bb
abelmez <abelmezaenriquez@outlook.com>
2026-08-30 01:29:50 -0700

    build(release): bump versionCode to 2 and build buscaminas_del_rancho_v2.aab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.nov0caina.buscaminas.estilo.sinaloa"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1 
-    versionName = "1.0.0-alpha"
+    versionCode = 2
+    versionName = "1.0.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
